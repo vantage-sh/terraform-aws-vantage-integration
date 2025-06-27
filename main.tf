e6f1e02cdffc7a8f6c7008648226e2f637af8245
Morre <morre@mor.re>
2025-06-27 11:37:15 +0200

    chore(deps): bump allowed AWS provider version
    This bumps the allowed version for the aws provider to allow version 6.0.0 and later to be used, too.
    
    Another possibility would be to completely remove the upper version constraint, but the change in this PR has fewer implications than the complete removal.
    Therefore, I decided to propose this initially.
    
    I'm happy to completely remove the upper version constraint if you'd prefer that.

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0.0, < 6.0.0"
+      version = ">= 4.0.0, < 7.0.0"
     }
   }
   required_version = ">= 1.0.0"
